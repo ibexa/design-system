b5a3df43088d4d80ecf430387da92a2eb0df2770
Michał Grabowski <michal.grabowski@ibexa.co>
2025-05-16 10:35:03 +0200

    Version 1.0.0-alpha1

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.xliff' {
-    const value: string;
-    export default value;
-};
-
-declare module '../packages/assets/src/img/all-icons.json' {
-    const icons: string[];
-    export default icons;
-};
