2ff608d3e60571cb4a3ffad91697b8941ea773e4
Michał Grabowski <michal.grabowski@ibexa.co>
2025-06-05 12:58:02 +0200

    Updated dependencies

--- packages/components/src/Accordion/Accordion.test.stories.tsx
+++ packages/components/src/Accordion/Accordion.test.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import type { Meta, StoryObj } from '@storybook/react';
-import { expect, fn, userEvent, within } from '@storybook/test';
+import { expect, fn, userEvent, within } from 'storybook/test';
 
 import { sleep } from '../../../../src/utils/test';
 
