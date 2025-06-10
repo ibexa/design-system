c22d0cfa70a48fbee9859a693c052f1e69ff3a9b
Michał Grabowski <michal.grabowski@ibexa.co>
2025-06-10 14:43:15 +0200

    Version 1.0.0-beta2

--- packages/components/src/Autosave/index.ts
+++
@@ -1,6 +0,0 @@
-import Autosave, { AUTOSAVE_STATUS } from './Autosave';
-import { AutosaveProps } from './Autosave.types';
-
-export default Autosave;
-export { AUTOSAVE_STATUS };
-export type { AutosaveProps };
--- src/storybook/addons/framework-selector/index.ts
+++
@@ -1,7 +0,0 @@
-export const previewAnnotations = (entry = []) => {
-    return [...entry, require.resolve('./preview.ts')];
-};
-
-export const managerEntries = (entry = []) => {
-    return [...entry, require.resolve('./manager.tsx')];
-};
