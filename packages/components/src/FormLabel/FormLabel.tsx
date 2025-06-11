dfd6613b9af104f4e1bea462cebd05fefb59c1fb
Michał Grabowski <michal.grabowski@ibexa.co>
2025-06-11 15:24:57 +0200

    form helper

--- packages/components/src/FormLabel/FormLabel.tsx
+++ packages/components/src/FormLabel/FormLabel.tsx
@@ -4,24 +4,16 @@ import { createCssClassNames } from '@ids-internal/shared/css.class.names';
 
 import { FormLabelProps } from './FormLabel.types';
 
-const FormLabel = ({
-    children,
-    htmlFor,
-    error = false,
-    extraAria = {},
-    extraClasses = '',
-    required = false,
-    title = '',
-}: FormLabelProps) => {
+const FormLabel = ({ children, htmlFor, error = false, extraClasses = '', required = false, title = '' }: FormLabelProps) => {
     const classes = createCssClassNames({
-        'ids-label': true,
-        'ids-label--error': error,
-        'ids-label--required': required,
+        'ids-form-label': true,
+        'ids-form-label--error': error,
+        'ids-form-label--required': required,
         [extraClasses]: true,
     });
 
     return (
-        <label className={classes} htmlFor={htmlFor} title={title} {...extraAria}>
+        <label className={classes} htmlFor={htmlFor} title={title}>
             {children}
         </label>
     );
