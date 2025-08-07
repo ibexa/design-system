ffa8a2c63f33e46ad599bba1c56b09d37d8923e2
Michał Grabowski <michal.grabowski@ibexa.co>
2025-08-07 16:31:50 +0200

    IBX-10471: [Inputs] Inputs Validators Base

---
+++ packages/components/src/validators/ValidatorManager.ts
@@ -0,0 +1,39 @@
+import { TranslatorType } from '@ids-context/Translator/Translator.types';
+
+import BaseValidator from './BaseValidator';
+
+interface ValidatorManagerOptions {
+    translator: TranslatorType;
+}
+
+export default class ValidatorManager {
+    private _validators: BaseValidator[];
+    private _translator: TranslatorType;
+
+    constructor(validators: BaseValidator[] = [], { translator }: ValidatorManagerOptions) {
+        validators.forEach((validator) => {
+            validator.setTranslator(translator);
+        });
+
+        this._validators = validators;
+        this._translator = translator;
+    }
+
+    addValidator(validator: BaseValidator): void {
+        validator.setTranslator(this._translator);
+
+        this._validators.push(validator);
+    }
+
+    removeValidator(validator: BaseValidator): void {
+        this._validators = this._validators.filter((savedValidator) => savedValidator !== validator);
+    }
+
+    validate(value: unknown) {
+        const errors = this._validators
+            .filter((validator: BaseValidator) => !validator.validate(value))
+            .map((validator: BaseValidator) => validator.getErrorMessage());
+
+        return { isValid: !errors.length, messages: errors };
+    }
+}
