935e0ce9bd1ec140ff9cb775587a514276cb489f
Michał Grabowski <michal.grabowski@ibexa.co>
2025-08-26 12:11:01 +0200

    removed unnecessary alias

--- scripts/symfony/create-dev-aliases.cjs
+++ scripts/symfony/create-dev-aliases.cjs
@@ -4,6 +4,5 @@ module.exports = (Encore) => {
     Encore.addAliases({
         '@ids-assets': path.resolve(__dirname, '../../packages/assets/src'),
         '@ids-components': path.resolve(__dirname, '../../packages/components/src'),
-        '@ids-core': path.resolve(__dirname, '../../packages/core/src'),
     });
 };
