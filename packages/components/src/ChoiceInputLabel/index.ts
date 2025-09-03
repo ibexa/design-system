cabed3f8158d74bff0b336844fd4e3c69973a213
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-03 15:41:39 +0200

    IBX-10604: ChoiceInput Label (#26)

---
+++ packages/components/src/ChoiceInputLabel/index.ts
@@ -0,0 +1,5 @@
+import ChoiceInputLabel from './ChoiceInputLabel';
+import { ChoiceInputLabelProps } from './ChoiceInputLabel.types';
+
+export default ChoiceInputLabel;
+export type { ChoiceInputLabelProps };
