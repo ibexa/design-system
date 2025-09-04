998cb2f87bb175efb4f5670b04f3a5d56b73e02a
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-05 00:04:06 +0200

    IBX-10619: Radio buttons list

--- packages/components/src/inputs/RadioButton/RadioButton.types.ts
+++ packages/components/src/inputs/RadioButton/RadioButton.types.ts
@@ -1,3 +1,5 @@
 import { BaseChoiceInputProps } from '@ids-internal/partials/BaseChoiceInput';
 
-export type RadioButtonProps = Omit<BaseChoiceInputProps, 'type'>;
+export type RadioButtonProps = Omit<BaseChoiceInputProps, 'type'> & {
+    value: string;
+};
