a95b72d0b785258640f79498c1dec7c3ed7537b0
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-15 15:38:46 +0200

    removed unneded commands

--- bin/prepare_ibexa_webpack.mjs
+++ bin/prepare_ibexa_webpack.mjs
@@ -3,20 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
 import { fileURLToPath } from 'url';
-import minimist from 'minimist';
 
-const getArgument = (argName, defaultValue = null) => {
-    const args = minimist(process.argv.slice(2));
-
-    return args[argName] || defaultValue;
-}
-const getScripts = (commandName, webpackFilename) => {
-    return {
-        [`${commandName}:dev`]: `encore dev --config ./${webpackFilename}`,
-        [`${commandName}:watch`]: `encore dev --watch --config ./${webpackFilename}`,
-        [`${commandName}:build`]: `encore production --config ./${webpackFilename}`,
-    }
-}
 const getSourceDir = (packageName) => {
     const currFileDir = path.dirname(fileURLToPath(import.meta.url));
     const dsDir = path.join(currFileDir, '..');
@@ -26,18 +13,6 @@ const getSourceDir = (packageName) => {
 const createSymlink = (source, target) => {
     execSync(`mkdir -p ${target} && rm -rf ${target} && ln -s ${source} ${target}`);
 }
-const updatePackageJsonFile = (webpackFilename, commandName) => {
-    const packageJsonFilePath = path.resolve('package.json');
-    const packageJsonContent = JSON.parse(fs.readFileSync(packageJsonFilePath, 'utf-8'));
-    const newScripts = getScripts(commandName, webpackFilename);
-
-    packageJsonContent.scripts = {
-        ...packageJsonContent.scripts,
-        ...newScripts,
-    };
-
-    fs.writeFileSync(packageJsonFilePath, JSON.stringify(packageJsonContent, null, 2));
-};
 const updateIbexaTSConfigFile = () => {
     const tsConfigFilePath = path.resolve('ibexa.tsconfig.json');
     const tsConfigContent = JSON.parse(fs.readFileSync(tsConfigFilePath, 'utf-8'));
@@ -60,20 +35,6 @@ const updateIbexaTSConfigFile = () => {
 
     fs.writeFileSync(tsConfigFilePath, JSON.stringify(tsConfigContent, null, 4));
 }
-const updateComposerJsonFile = (commandName) => {
-    const composerJsonFilePath = path.resolve('composer.json');
-    const composerJsonContent = JSON.parse(fs.readFileSync(composerJsonFilePath, 'utf-8'));
-    const oldCommand = 'ibexa:encore:compile --frontend-configs-name ibexa,internals,libs,richtext';
-    const newCommand = 'ibexa:encore:compile --frontend-configs-name internals,libs,richtext';
-    const dsCommand = `yarn ${commandName}:dev`;
-
-    delete composerJsonContent.scripts['auto-scripts'][oldCommand];
-
-    composerJsonContent.scripts['auto-scripts'][newCommand] = 'symfony-cmd';
-    composerJsonContent.scripts['auto-scripts'][dsCommand] = 'script';
-
-    fs.writeFileSync(composerJsonFilePath, JSON.stringify(composerJsonContent, null, 4));
-}
 const createSymlinks = () => {
     const composerJsonFilePath = path.resolve('composer.json');
     const composerJsonContent = JSON.parse(fs.readFileSync(composerJsonFilePath, 'utf-8'));
@@ -91,33 +52,6 @@ const createSymlinks = () => {
     createSymlink(componentsSourcePath, componentsSymlinkPath);
     createSymlink(coreSourcePath, coreSymlinkPath);
 }
-const createWebpackConfigFile = (filePath) => {
-    const webpackConfigFileContent = `
-const path = require('path');
-const allConfigs = require('@ibexa/frontend-config/webpack-config/all');
-const ibexaConfig = allConfigs[0]();
 
-allConfigs[0] = () => {
-    ibexaConfig.resolve.alias['@ids-assets'] = '${getSourceDir('assets')}';
-    ibexaConfig.resolve.alias['@ids-components'] = '${getSourceDir('components')}';
-    ibexaConfig.resolve.alias['@ids-core'] = '${getSourceDir('core')}';
-
-    return ibexaConfig;
-};
-
-module.exports = allConfigs;
-
-`;
-
-    fs.writeFileSync(filePath, webpackConfigFileContent, 'utf-8');
-};
-
-const commandDevName = getArgument('command', 'ibexa-ds');
-const ibexaWebpackConfigFilename = getArgument('webpack-file', 'ibexa.webpack.config.js');
-const ibexaWebpackConfigFilePath = path.resolve(ibexaWebpackConfigFilename);
-
-updatePackageJsonFile(ibexaWebpackConfigFilename, commandDevName);
-createWebpackConfigFile(ibexaWebpackConfigFilePath);
-updateComposerJsonFile(commandDevName);
 updateIbexaTSConfigFile();
 createSymlinks();
