6c3e4ad7f2522fec03bc4ee70f1cbbc197fc45d4
Aleksandra Bożek <aleksandra.m.bozek@gmail.com>
2025-09-16 14:32:45 +0200

    IBX-7880: Badge (#19)

---
+++ packages/components/src/Badge/Badge.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Badge, { BadgeSize } from './';
+
+const meta: Meta<typeof Badge> = {
+    component: Badge,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs', 'foundation'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {
+    name: 'Default',
+    args: {
+        value: 1,
+    },
+};
+
+export const Small: Story = {
+    name: 'Small',
+    args: {
+        value: 1,
+        size: BadgeSize.Small,
+    },
+};
+
+export const Stretched: Story = {
+    name: 'Stretched',
+    args: {
+        value: 100,
+        size: BadgeSize.Medium,
+    },
+};
