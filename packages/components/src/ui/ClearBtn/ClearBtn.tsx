7fc531ca0d6f873db15dd1bcc3d120d488ff21e7
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-24 15:32:29 +0200

    Add translations (#52)

--- packages/components/src/ui/ClearBtn/ClearBtn.tsx
+++ packages/components/src/ui/ClearBtn/ClearBtn.tsx
@@ -8,7 +8,7 @@ import { ClearBtnProps } from './ClearBtn.types';
 
 export const ClearBtn = ({ onClick, disabled = false }: ClearBtnProps) => {
     const Translator = useContext(TranslatorContext);
-    const clearMsg = Translator.trans(/*@Desc("Clear")*/ 'ibexa.clear-btn.label');
+    const clearMsg = Translator.trans(/*@Desc("Clear")*/ 'ids.clear_btn.label');
     const componentClassName = createCssClassNames({
         'ids-clear-btn': true,
     });
