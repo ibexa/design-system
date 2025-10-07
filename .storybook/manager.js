de7adee38b4c56bd2caceafb64041aae70671c11
Michał Grabowski <michal.grabowski@ibexa.co>
2025-10-07 11:07:36 +0200

    Version 1.0.1

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from 'storybook/manager-api';
-import IbexaTheme from './IbexaTheme';
-
-addons.setConfig({
-    theme: IbexaTheme,
-});
--- types/xliff/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'xliff' {
-    type xliff12ToJsType = (content: string) => Promise<any>;
-
-    let xliff12ToJs: xliff12ToJsType;
-};
