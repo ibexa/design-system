7a689bc63fac2b10fa9eea8eaa0d4776759fbcac
Aleksandra Bozek <aleksandra.m.bozek@gmail.com>
2025-10-15 15:11:37 +0200

    Update imports

--- packages/components/src/components/Tag/Tag.tsx
+++ packages/components/src/components/Tag/Tag.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Icon, IconSize } from '@ids-components/Icon';
-import { createCssClassNames } from '@ibexa/ids-core/helpers/cssClassNames';
+import { createCssClassNames } from '@ids-core/helpers/cssClassNames';
 
 import { TagGhostType, TagProps, TagSize, TagType } from './Tag.types';
 
