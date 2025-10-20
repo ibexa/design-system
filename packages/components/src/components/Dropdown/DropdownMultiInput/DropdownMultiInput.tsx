d83774067d7cc02d229e2a0758ade71b29f6c1a7
Michał Grabowski <michal.grabowski@ibexa.co>
2025-10-20 08:52:55 +0200

    IBX-7908: Dropdown - multivalue (#55)

---
+++ packages/components/src/components/Dropdown/DropdownMultiInput/DropdownMultiInput.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import { BaseDropdown } from '@ids-partials/BaseDropdown';
+import { createCssClassNames } from '@ids-core/helpers/cssClassNames';
+import withStateValue from '@ids-hoc/withStateValue';
+
+import { DropdownMultiInputAction, DropdownMultiInputItem, DropdownMultiInputProps } from './DropdownMultiInput.types';
+import { CheckboxInput } from '@ids-components/Checkbox';
+
+export const DropdownMultiInput = ({
+    name,
+    className = '',
+    items = [],
+    onChange = () => undefined,
+    value = [],
+    ...restProps
+}: DropdownMultiInputProps) => {
+    const dropdownClassName = createCssClassNames({
+        'ids-dropdown--multi': true,
+        [className]: !!className,
+    });
+    const changeValue = (id: string) => {
+        const oldValueLength = value.length;
+        const newValue = value.includes(id) ? value.filter((val) => val !== id) : [...value, id];
+        const actionPerformed = newValue.length > oldValueLength ? DropdownMultiInputAction.Check : DropdownMultiInputAction.Uncheck;
+
+        onChange(newValue, id, actionPerformed);
+    };
+    const clickDropdownItem = ({ id }: DropdownMultiInputItem) => {
+        changeValue(id);
+    };
+    const getItemAttributes = () => {
+        return {
+            role: undefined,
+            tabIndex: undefined,
+        };
+    };
+    const isItemSelected = (item: DropdownMultiInputItem) => value.includes(item.id);
+    const renderItem = (item: DropdownMultiInputItem) => {
+        return (
+            <>
+                <CheckboxInput checked={isItemSelected(item)} name={`${name}-checkbox`} value={item.id} />
+                {item.label}
+            </>
+        );
+    };
+    const selectedItems = value.length ? items.filter((item) => value.includes(item.id)) : [];
+    const renderSelectedItems = () => (
+        <>{selectedItems.map((item) => item.label).join(', ')}</>
+        // TODO: replace with chips when done
+    );
+    const renderSource = () => {
+        return (
+            <select defaultValue={value} multiple name={name} tabIndex={-1}>
+                {items.map((item) => (
+                    <option key={item.id} value={item.id}>
+                        {item.label}
+                    </option>
+                ))}
+            </select>
+        );
+    };
+
+    return (
+        <BaseDropdown
+            {...restProps}
+            className={dropdownClassName}
+            getItemAttributes={getItemAttributes}
+            isEmpty={selectedItems.length === 0}
+            isItemSelected={isItemSelected}
+            items={items}
+            onDropdownItemClick={clickDropdownItem}
+            renderItem={renderItem}
+            renderSelectedItems={renderSelectedItems}
+            renderSource={renderSource}
+        />
+    );
+};
+
+export const DropdownMultiInputStateful = withStateValue<DropdownMultiInputProps, string[]>(DropdownMultiInput);
---
+++ packages/components/src/components/Dropdown/DropdownMultiInput/DropdownMultiInput.types.ts
@@ -0,0 +1,18 @@
+import { BaseComponentAttributes } from '@ids-types/general';
+import { BaseDropdownItem } from '@ids-partials/BaseDropdown';
+
+export type DropdownMultiInputItem = BaseDropdownItem;
+
+export enum DropdownMultiInputAction {
+    Check = 'check',
+    Uncheck = 'uncheck',
+}
+
+export interface DropdownMultiInputProps extends BaseComponentAttributes {
+    name: string;
+    disabled?: boolean;
+    error?: boolean;
+    items?: DropdownMultiInputItem[];
+    onChange?: (value: string[], itemValue: string, action: DropdownMultiInputAction) => void;
+    value?: string[];
+}
