cd77299fdc5f1557efc265027c92cf1097402c13
Michał Grabowski <michal.grabowski@ibexa.co>
2025-10-27 08:11:32 +0100

    IBX-10836: Fix focus in dropdown (#71)

--- packages/components/src/partials/BaseDropdown/components/ItemsContainer/ItemsContainer.types.ts
+++ packages/components/src/partials/BaseDropdown/components/ItemsContainer/ItemsContainer.types.ts
@@ -1,5 +1,16 @@
 import { BaseDropdownItem, ExtraDropdownItemClickParamsType } from '../../BaseDropdown.types';
 
+export enum ItemsContainerMoveActiveFocusDirection {
+    Up = 'up',
+    Down = 'down',
+}
+
+export type GetNextFocusableItemType = (
+    element: HTMLElement,
+    direction: ItemsContainerMoveActiveFocusDirection,
+    { itemsList, search }: { itemsList: HTMLUListElement; search: HTMLInputElement | null },
+) => HTMLElement | null;
+
 export type ItemsContainerItemsStylesType = React.CSSProperties & {
     '--max-visible-items'?: number;
 };
@@ -8,6 +19,7 @@ export interface ItemsContainerProps<T extends BaseDropdownItem> {
     closeDropdown: () => void;
     filterFunction: (item: T, searchTerm: string) => boolean;
     getItemAttributes: (item: T) => React.HTMLAttributes<HTMLElement>;
+    getNextFocusableItem: GetNextFocusableItemType;
     isItemSelected: (item: T) => boolean;
     isOpen: boolean;
     items: T[];
