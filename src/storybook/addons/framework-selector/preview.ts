4eaa76ba817e6d3cb4dd13791dffe32f799d0c78
Michał Grabowski <michal.grabowski@ibexa.co>
2026-03-03 12:37:25 +0100

    Version 1.0.3

--- src/storybook/addons/framework-selector/preview.ts
+++
@@ -1,7 +0,0 @@
-import { FRAMEWORK, KEY } from './constants';
-import FrameworkSelectorDecorator from './FrameworkSelectorDecorator';
-
-export const decorators = [FrameworkSelectorDecorator];
-export const initialGlobals = {
-    [KEY]: FRAMEWORK.REACT,
-};
