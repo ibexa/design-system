0d9b0fb2ddd6fe4542208bd04d9119408ebf48c7
Michał Grabowski <michal.grabowski@ibexa.co>
2026-04-13 12:51:05 +0200

    Version 1.0.5

--- packages/core/src/helpers/escape.ts
+++
@@ -1,7 +0,0 @@
-export const escapeHTML = (string: string) => {
-    const stringTempNode = document.createElement('div');
-
-    stringTempNode.appendChild(document.createTextNode(string));
-
-    return stringTempNode.innerHTML;
-};
