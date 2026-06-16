f6762a0778bfbe37daefbb8e1d1c0771a55df7af
Michał Grabowski <michal.grabowski@ibexa.co>
2026-06-16 12:23:18 +0200

    Version 2.0.1

--- src/storybook/addons/framework-selector/index.ts
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from 'url';
-
-export const previewAnnotations = (entry = []) => {
-    return [...entry, fileURLToPath(import.meta.resolve('./preview.ts'))];
-};
-
-export const managerEntries = (entry = []) => {
-    return [...entry, fileURLToPath(import.meta.resolve('./manager.tsx'))];
-};
