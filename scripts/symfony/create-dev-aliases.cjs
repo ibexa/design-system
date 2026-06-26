d9fc70ec210c4685e957aa8c2fedc7f7a79aecbe
Michał Grabowski <michal.grabowski@ibexa.co>
2026-06-26 11:01:03 +0200

    Version 2.0.2

--- scripts/symfony/create-dev-aliases.cjs
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = (Encore) => {
-    Encore.addAliases({
-        '@ids-assets': path.resolve(__dirname, '../../packages/assets/src'),
-        '@ids-components': path.resolve(__dirname, '../../packages/components/src'),
-    });
-};
